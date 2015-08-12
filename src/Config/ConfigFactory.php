49ffdf37df66bbb42c566cfb0c36ff03cd70846e
HappyRay <ruiguo@gmail.com>
2015-08-12 07:32:18 -0700

    Add new implementations of GlobalComputedConfig and ConfigFactory

---
+++ src/Config/ConfigFactory.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ */
+
+namespace Box\TestScribe\Config;
+
+use Box\TestScribe\MethodHelper;
+use Box\TestScribe\PhpClass;
+use Symfony\Component\Console\Input\InputInterface;
+
+/**
+ * Class ConfigFactory
+ *
+ * Calculate global configurations.
+ *
+ * @var InputConfig|OptionsConfig|OutputConfig|MethodHelper
+ */
+class ConfigFactory
+{
+    /** @var InputConfig */
+    private $inputConfig;
+
+    /** @var OptionsConfig */
+    private $optionsConfig;
+
+    /** @var OutputConfig */
+    private $outputConfig;
+
+    /** @var MethodHelper */
+    private $methodHelper;
+
+    /**
+     * @param \Box\TestScribe\Config\InputConfig   $inputConfig
+     * @param \Box\TestScribe\Config\OptionsConfig $optionsConfig
+     * @param \Box\TestScribe\Config\OutputConfig  $outputConfig
+     * @param \Box\TestScribe\MethodHelper         $methodHelper
+     */
+    function __construct(
+        InputConfig $inputConfig,
+        OptionsConfig $optionsConfig,
+        OutputConfig $outputConfig,
+        MethodHelper $methodHelper
+    )
+    {
+        $this->inputConfig = $inputConfig;
+        $this->optionsConfig = $optionsConfig;
+        $this->outputConfig = $outputConfig;
+        $this->methodHelper = $methodHelper;
+    }
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     *
+     * @return \Box\TestScribe\GlobalComputedConfig
+     */
+    public function config(InputInterface $input)
+    {
+        $inputParams = $this->inputConfig->getInputParams($input);
+
+        $inSourceFile = $inputParams->getSourceFile();
+        $options = $this->optionsConfig->getOptions($input, $inSourceFile);
+
+        $outputParams = $this->outputConfig->getOutputParams(
+            $options,
+            $inputParams
+        );
+
+        $inPhpClassName = $inputParams->getPhpClassName();
+        $inPhpClass = new PhpClass($inPhpClassName);
+        $methodName = $inputParams->getMethodName();
+        $inMethodObj = $this->methodHelper->createFromMethodName($inPhpClass, $methodName);
+
+        $config = new GlobalComputedConfig(
+            $inputParams,
+            $inMethodObj,
+            $options,
+            $outputParams
+        );
+
+        return $config;
+    }
+}
---
+++ src/Config/GlobalComputedConfig.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace Box\TestScribe\Config;
+
+use Box\TestScribe\App;
+use Box\TestScribe\Method;
+use Box\TestScribe\PhpClassName;
+
+/**
+ * Class GlobalComputedConfig
+ * @package Box\TestScribe
+ *
+ * Computed configuration values
+ * including direct user configuration and derived configuration values.
+ */
+class GlobalComputedConfig
+{
+    /**
+     * @var PhpClassName
+     *
+     * The class being tested.
+     */
+    private $inPhpClassName;
+
+    /**
+     * The method under test.
+     *
+     * @var Method
+     */
+    private $inMethod;
+
+    /**
+     * @var string
+     *
+     * The source file path of the file that contains the class being tested.
+     */
+    private $inSourceFile;
+
+    /**
+     * @var PhpClassName
+     *
+     * The name of the output test class name.
+     * Note that this class is yet to be generated.
+     * So it can't be represented by PhpClass class.
+     */
+    private $outPhpClassName;
+
+    /**
+     * @var string
+     *
+     * The path of the file that contains the generated tests.
+     */
+    private $outSourceFile;
+
+    /**
+     * The name of the test to generate.
+     *
+     * @var string
+     */
+    private $outMethodName;
+
+    /**
+     * @var bool
+     */
+    private $overwriteExistingDestinationFile;
+
+    /**
+     * @param \Box\TestScribe\Config\ConfigParams $inputParams
+     * @param \Box\TestScribe\Method              $inMethod
+     * @param \Box\TestScribe\Config\Options      $options
+     * @param \Box\TestScribe\Config\ConfigParams $outputParams
+     */
+    function __construct(
+        ConfigParams $inputParams,
+        Method $inMethod,
+        Options $options,
+        ConfigParams $outputParams
+    )
+    {
+        $this->inPhpClassName = $inputParams->getPhpClassName();
+        $this->inMethod = $inMethod;
+        $this->inSourceFile = $inputParams->getSourceFile();
+        $this->outPhpClassName = $outputParams->getPhpClassName();
+        $this->outSourceFile = $outputParams->getSourceFile();
+        $this->outMethodName = $outputParams->getMethodName();
+        $this->overwriteExistingDestinationFile = $options->isOverwriteExistingDestinationFile();
+    }
+
+    /**
+     * Get the return type of the method under test.
+     *
+     * @return \Box\TestScribe\PHPDoc\PHPDocType
+     */
+    public function getReturnType()
+    {
+        $returnType = $this->inMethod->getReturnType();
+
+        return $returnType;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReturnTypeVoid()
+    {
+        $returnType = $this->inMethod->getReturnType();
+
+        $isVoid = $returnType->isVoid();
+
+        return $isVoid;
+    }
+
+    /**
+     * Get the full class name of the class under test.
+     *
+     * @return string
+     */
+    public function getFullClassName()
+    {
+        $fullName = $this->inPhpClassName->getFullyQualifiedClassName();
+
+        return $fullName;
+    }
+
+    /**
+     * @return \Box\TestScribe\Method
+     */
+    public function getInMethod()
+    {
+        return $this->inMethod;
+    }
+
+    /**
+     * Get the name of the method under test.
+     *
+     * @return string
+     */
+    public function getMethodName()
+    {
+        return $this->inMethod->getName();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isMethodStatic()
+    {
+        $methodObj = $this->inMethod;
+
+        $isStatic = $methodObj->isStatic();
+
+        return $isStatic;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isMethodPublic()
+    {
+        $methodObj = $this->inMethod;
+
+        $isStatic = $methodObj->isPublic();
+
+        return $isStatic;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getInSourceFile()
+    {
+        return $this->inSourceFile;
+    }
+
+    /**
+     * @return \Box\TestScribe\PhpClassName
+     */
+    public function getOutPhpClassName()
+    {
+        return $this->outPhpClassName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOutSourceFile()
+    {
+        return $this->outSourceFile;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTestMethodName()
+    {
+        return $this->outMethodName;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isOverwriteExistingDestinationFile()
+    {
+        return $this->overwriteExistingDestinationFile;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInjectMockedObjectMethodName()
+    {
+        $name = App::getInjectMockedObjectMethodName();
+
+        return $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInjectMockedClassMethodName()
+    {
+        $name = App::getInjectMockedClassMethodName();
+
+        return $name;
+    }
+}
