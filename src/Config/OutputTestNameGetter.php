a41a2867ba874893b4b97989b6ec552235bf0422
HappyRay <ruiguo@gmail.com>
2015-08-11 19:21:50 -0700

    Add an alternative implementation of getting the output test name
    The new one uses dependency injection.

---
+++ src/Config/OutputTestNameGetter.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace Box\TestScribe\Config;
+
+use Box\TestScribe\GeneratorException;
+use Box\TestScribe\Output;
+use Box\TestScribe\RawInputWithPrompt;
+
+
+/**
+ * @var RawInputWithPrompt|Output
+ */
+class OutputTestNameGetter 
+{
+    /** @var RawInputWithPrompt */
+    private $rawInputWithPrompt;
+
+    /** @var Output */
+    private $output;
+
+    /**
+     * @param \Box\TestScribe\RawInputWithPrompt $rawInputWithPrompt
+     * @param \Box\TestScribe\Output             $output
+     */
+    function __construct(
+        RawInputWithPrompt $rawInputWithPrompt,
+        Output $output
+    )
+    {
+        $this->rawInputWithPrompt = $rawInputWithPrompt;
+        $this->output = $output;
+    }
+
+    /**
+      * Get the name of the test to create.
+      * Method has to begin with 'test'
+      *
+      * @param string $className
+      * @param string $methodName
+      * @param bool   $useDefaultTestMethodName
+      *
+      * @throws GeneratorException
+      * @return string
+      */
+     public function getTestName(
+         $className,
+         $methodName,
+         $useDefaultTestMethodName
+     )
+     {
+         // Convert the first letter of the method name to upper case
+         // to make the result a valid camel case.
+         $defaultTestMethodName = 'test' . ucfirst($methodName);
+
+         if ($useDefaultTestMethodName) {
+             return $defaultTestMethodName;
+         }
+
+         $message =
+             "Enter the name of the test for the method ( $methodName ) of the class ( $className ).\n"
+             . "Press enter to use the default test name ( $defaultTestMethodName ).";
+
+         $this->output->writeln($message);
+
+         // rawInput is used instead of InputWithHelp so that
+         // users don't have to quote the name as instructed by the help.
+         $input = $this->rawInputWithPrompt->getString($message);
+         if ($input === '') {
+             return $defaultTestMethodName;
+         }
+         if (0 !== strpos($input, 'test')) {
+             $error =
+                 "Test method must begin with the string 'test'. Please try again.";
+             throw new GeneratorException($error);
+         }
+
+         return $input;
+     }
+}
