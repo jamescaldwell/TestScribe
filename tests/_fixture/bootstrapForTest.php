ff94cdc4dcf05c45695524ce90fefe5508627ae9
HappyRay <ruiguo@gmail.com>
2015-09-17 16:27:27 -0700

    Escape the '\' in class names in the bootstrap file for TestScribe testing itself

--- tests/_fixture/bootstrapForTest.php
+++ tests/_fixture/bootstrapForTest.php
@@ -9,10 +9,10 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 // Instruct the test generator which methods to use to
 // generate the mock object registration/injection statement.
 \Box\TestScribe\App::setInjectMockedObjectMethodName(
-    '\Box\TestScribe\_fixture\ServiceLocator::overwrite'
+    '\\Box\\TestScribe\\_fixture\\ServiceLocator::overwrite'
 );
 \Box\TestScribe\App::setInjectMockedClassMethodName(
-    '\Box\TestScribe\_fixture\StaticServiceLocator::overwrite'
+    '\\Box\\TestScribe\\_fixture\\StaticServiceLocator::overwrite'
 );
 
 Directory::init();
