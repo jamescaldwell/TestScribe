6256852f4da0e5faf26588b7e3a23a0f526ea9c5
HappyRay <ruiguo@gmail.com>
2015-09-29 22:04:48 -0700

    Use the saved method parameter value if available

--- src/ArgumentInfo/ArgumentsCollector.php
+++ src/ArgumentInfo/ArgumentsCollector.php
@@ -5,36 +5,54 @@
 
 namespace Box\TestScribe\ArgumentInfo;
 
+use Box\TestScribe\Config\GlobalComputedConfig;
+use Box\TestScribe\Input\InputValueFactory;
 use Box\TestScribe\Input\InputValueGetter;
 use Box\TestScribe\MethodInfo\Method;
 use Box\TestScribe\Output;
+use Box\TestScribe\Spec\SavedSpecs;
 
 /**
  * Collect arguments to a method.
+ * @var Output|InputValueGetter|SavedSpecs|GlobalComputedConfig|InputValueFactory
  */
 class ArgumentsCollector
 {
-    /**
-     * @var Output
-     */
-    private $out;
+    /** @var Output */
+    private $output;
+
+    /** @var InputValueGetter */
+    private $inputValueGetter;
+
+    /** @var SavedSpecs */
+    private $savedSpecs;
+
+    /** @var GlobalComputedConfig */
+    private $globalComputedConfig;
+
+    /** @var InputValueFactory */
+    private $inputValueFactory;
 
     /**
-     * @var InputValueGetter
-     */
-    private $inputValueGetterService;
-
-    /**
-     * @param \Box\TestScribe\Output           $out
-     * @param \Box\TestScribe\Input\InputValueGetter $inputValueGetterService
+     * @param \Box\TestScribe\Output $output
+     * @param \Box\TestScribe\Input\InputValueGetter $inputValueGetter
+     * @param \Box\TestScribe\Spec\SavedSpecs $savedSpecs
+     * @param \Box\TestScribe\Config\GlobalComputedConfig $globalComputedConfig
+     * @param \Box\TestScribe\Input\InputValueFactory $inputValueFactory
      */
     function __construct(
-        Output $out,
-        InputValueGetter $inputValueGetterService
+        Output $output,
+        InputValueGetter $inputValueGetter,
+        SavedSpecs $savedSpecs,
+        GlobalComputedConfig $globalComputedConfig,
+        InputValueFactory $inputValueFactory
     )
     {
-        $this->out = $out;
-        $this->inputValueGetterService = $inputValueGetterService;
+        $this->output = $output;
+        $this->inputValueGetter = $inputValueGetter;
+        $this->savedSpecs = $savedSpecs;
+        $this->globalComputedConfig = $globalComputedConfig;
+        $this->inputValueFactory = $inputValueFactory;
     }
 
     /**
@@ -58,9 +76,19 @@ class ArgumentsCollector
         $className = $reflectionMethod->getDeclaringClass()->getName();
         $message =
             "\nPrepare to get arguments to the method ( $methodName ) of the class ( $className ).\n";
-        $this->out->writeln($message);
+        $this->output->writeln($message);
+
+        $methodParams = null;
+        if (!$method->isConstructor()) {
+            $testName = $this->globalComputedConfig->getTestMethodName();
+            $savedSpec = $this->savedSpecs->getSpecForTest($testName);
+            if ($savedSpec) {
+                $methodParams = $savedSpec->getMethodParameters();
+            }
+        }
 
         $argsArray = [];
+        $index = 0;
         foreach ($args as $arg) {
             $argumentName = $arg->getName();
             $argPromptSubject = "parameter ( $argumentName )";
@@ -68,24 +96,32 @@ class ArgumentsCollector
             if ($isOptional) {
                 $argPromptSubject = "optional $argPromptSubject";
             }
-            $typeInfo = $method->getParamTypeString($argumentName);
-            $inputValue = $this->inputValueGetterService->get(
-                $typeInfo,
-                $argPromptSubject,
-                '',
-                $methodName,
-                $argumentName
-            );
+
+            if ($methodParams){
+                $value = $methodParams[$index];
+                $this->output->writeln("Get ( $value ) from the saved test for $argPromptSubject");
+                $inputValue = $this->inputValueFactory->createPrimitiveValue($value);
+            } else {
+                $typeInfo = $method->getParamTypeString($argumentName);
+                $inputValue = $this->inputValueGetter->get(
+                    $typeInfo,
+                    $argPromptSubject,
+                    '',
+                    $methodName,
+                    $argumentName
+                );
+            }
             if ($inputValue->isVoid()) {
                 // @TODO (ryang 1/9/15) : double check if the parameter is optional.
                 // Assume the parameters after this one will all have to be void.
                 break;
             }
             $argsArray[] = $inputValue;
+            $index++;
         }
 
         // Add an empty line for improved readability.
-        $this->out->writeln('');
+        $this->output->writeln('');
 
         $args = new Arguments($argsArray);
 
