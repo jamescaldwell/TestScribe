bb55f41c44e9bd0fe58415cd89de0ef49155c066
HappyRay <ruiguo@gmail.com>
2015-10-02 13:19:38 -0700

    Update the version to 0.3.6

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Application as AbstractApplication;
  */
 class Application extends AbstractApplication
 {
-    const APP_VERSION = '0.3.5';
+    const APP_VERSION = '0.3.6';
 
     /**
      *
