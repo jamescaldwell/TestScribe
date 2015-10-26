48c55732fca126c816b59e77c88f4c26b6a320b2
HappyRay <ruiguo@gmail.com>
2015-10-26 10:37:39 -0700

    Add a parameter to allow mock objects to be replaced with the mock class name

--- src/Utils/ValueTransformer.php
+++ src/Utils/ValueTransformer.php
@@ -10,68 +10,84 @@ use Box\TestScribe\Mock\MockClass;
 /**
  * Class ValueTransformer
  */
-class ValueTransformer 
+class ValueTransformer
 {
     /**
-      * @param mixed $value
-      *
-      * @return mixed
-      *
-      * Recursively translate any object or resource contained in this value to its string representation.
-      */
-     public function translateObjectsAndResourceToString($value)
-     {
-         if (is_object($value)) {
-             $result = $this->translateObjectToString($value);
+     * @param mixed $value
+     * @param bool $replaceMockObjectWithClassName
+     * true if full mock class name should be used instead of a more user readable name.
+     *
+     * @return mixed Recursively translate any object or resource contained in
+     * this value to its string representation.
+     *
+     * Recursively translate any object or resource
+     * contained in this value to its string representation.
+     */
+    public function translateObjectsAndResourceToString(
+        $value,
+        $replaceMockObjectWithClassName = false
+    )
+    {
+        if (is_object($value)) {
+            $result = $this->translateObjectToString($value, $replaceMockObjectWithClassName);
 
-             return $result;
-         }
+            return $result;
+        }
 
-         if (is_array($value)) {
-             $simpleArray = [];
-             foreach ($value as $index => $element) {
-                 $simpleArray[$index] = $this->translateObjectsAndResourceToString($element);
-             }
+        if (is_array($value)) {
+            $simpleArray = [];
+            foreach ($value as $index => $element) {
+                $simpleArray[$index] =
+                    $this->translateObjectsAndResourceToString(
+                        $element,
+                        $replaceMockObjectWithClassName
+                    );
+            }
 
-             return $simpleArray;
-         }
+            return $simpleArray;
+        }
 
-         if (is_resource($value)){
-             $type = get_resource_type($value);
-             $result = "resource ( $type )";
+        if (is_resource($value)) {
+            $type = get_resource_type($value);
+            $result = "resource ( $type )";
 
-             return $result;
+            return $result;
+        }
 
-         }
+        return $value;
+    }
 
-         return $value;
-     }
+    /**
+     * @param object $value
+     *
+     * @param bool $replaceMockObjectWithClassName
+     *
+     * @return string
+     */
+    private function translateObjectToString($value, $replaceMockObjectWithClassName)
+    {
+        $objectTypeString = get_class($value);
+        $mockClassUtil = new MockClassUtil();
+        $isMockObject = $mockClassUtil->isMockClass($objectTypeString);
+        if ($isMockObject) {
+            // This object must support MockTrait which includes this method.
+            /* @var MockClass $mockObj */
+            $mockObj = $value->__getUnitTestGeneratorMockInstance();
+            if ($replaceMockObjectWithClassName){
+                $result = $mockObj->getClassNameBeingMocked();
+            } else {
+                $result = $mockObj->__toString();
+            }
+        } else {
+            $result = "( $objectTypeString ) object";
 
-     /**
-      * @param object $value
-      *
-      * @return string
-      */
-     private function translateObjectToString($value)
-     {
-         $objectTypeString = get_class($value);
-         $mockClassUtil = new MockClassUtil();
-         $isMockObject = $mockClassUtil->isMockClass($objectTypeString);
-         if ($isMockObject) {
-             // This object must support MockTrait which includes this method.
-             /* @var MockClass $mockObj */
-             $mockObj = $value->__getUnitTestGeneratorMockInstance();
-             $result = $mockObj->__toString();
-         } else {
-             $result = "( $objectTypeString ) object";
+            if ($value instanceof \JsonSerializable) {
+                $jsonEncoded = json_encode($value);
+                $result .= " value ( $jsonEncoded )";
+            }
+        }
 
-             if ($value instanceof \JsonSerializable) {
-                 $jsonEncoded = json_encode($value);
-                 $result .= " value ( $jsonEncoded )";
-             }
-         }
-
-         return $result;
-     }
+        return $result;
+    }
 
 }
