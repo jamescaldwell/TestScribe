9b003d0a1e73083fc1e4795a7756dc8b846f6f06
HappyRay <ruiguo@gmail.com>
2015-11-03 10:18:13 -0800

    Add a mockMgr class to hold all the mock objects

---
+++ src/Mock/MockMgr.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Box\TestScribe\Mock;
+
+/**
+ * Hold information about all mocks.
+ */
+class MockMgr
+{
+    /** @var  MockClass[] */
+    private $mocks;
+
+    /**
+     * @param \Box\TestScribe\Mock\MockClass $mock
+     * @return void
+     */
+    public function addMock(MockClass $mock)
+    {
+        $this->mocks[] = $mock;
+    }
+
+    /**
+     * @return \Box\TestScribe\Mock\MockClass[]
+     */
+    public function getMocks()
+    {
+        return $this->mocks;
+    }
+}
